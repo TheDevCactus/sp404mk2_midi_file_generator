94ffea3ceb279ccea9763e54ba70e43eb196344e
Morgan Brown <morganb816@gmail.com>
2024-05-16 10:21:37 -0400

    chore: im stupid

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/sp404mk2_midi_file_generator/",
   build: {
-    outDir: "../docs",
+    outDir: "./docs",
     assetsDir: "assets",
   },
 });
